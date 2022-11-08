835ad916c47aa983d9cc43c03b8d9983ee9f90cd
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-08 17:36:45 +0100

    Create guest layout

---
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -0,0 +1,23 @@
+<template>
+  <guest-layout>
+    <div class="flex justify-end m-2 p-2">
+      <Link
+        v-if="$page.props.auth.auth_check"
+        :href="route('communities.posts.create', community.slug)"
+        class="px-4 py-2 rounded bg-indigo-500 hover:bg-indigo-600 text-white"
+      >
+        Create Post
+      </Link>
+    </div>
+    <h1>Community {{ community.name }} show page</h1>
+  </guest-layout>
+</template>
+
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+
+defineProps({
+  community: Object(),
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,8 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\Backend\CommunityController;
-use App\Http\Controllers\Frontend\SubredditController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Backend\CommunityPostController;
+use App\Http\Controllers\Frontend\CommunityController as FrontendCommunityController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -26,7 +16,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/r/{slug}', [SubredditController::class, 'show'])->name('subbredit.show');
+Route::get('/r/{slug}', [FrontendCommunityController::class, 'show'])->name('subbredit.show');
 
 Route::group(['middleware' => ['auth', 'verified']], function (){
     Route::get('/dashboard', function () {
@@ -34,6 +24,7 @@ Route::group(['middleware' => ['auth', 'verified']], function (){
     })->name('dashboard');
 
     Route::resource('/dashboard/communities', CommunityController::class);
+    Route::resource('/dashboard/communities.posts', CommunityPostController::class);
 });
 
 require __DIR__.'/auth.php';
