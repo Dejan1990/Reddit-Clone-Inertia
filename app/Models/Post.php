b0037c5b81eef163f47a646124eb86671e1d15f7
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-08 13:00:58 +0100

    Created Models and Migrations

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'community_id',
+        'title',
+        'slug',
+        'description',
+        'url'
+    ];
+}
