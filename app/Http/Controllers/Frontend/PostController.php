081f73e804f7aa5f0ce7885935378461bb0da4b4
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-11 21:30:31 +0100

    admin and moderator

--- app/Http/Controllers/Frontend/PostController.php
+++ app/Http/Controllers/Frontend/PostController.php
@@ -8,6 +8,7 @@ use App\Models\Community;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\PostShowResource;
 
 class PostController extends Controller
@@ -16,12 +17,17 @@ class PostController extends Controller
     {
         $community = Community::where('slug', $community_slug)->first();
 
-        $post = new PostShowResource(Post::with(['comments', 'postVotes' => function ($query) {
+        $community_post = Post::with(['comments', 'postVotes' => function ($query) {
             $query->where('user_id', auth()->id());
-        }])->where('slug', $slug)->first());
+        }])->where('slug', $slug)->first();
+
+        $post = new PostShowResource($community_post);
 
         $posts = PostResource::collection($community->posts()->orderBy('votes', 'desc')->take(6)->get());
+
+        $can_update = Auth::user()->can('update', $community_post);
+        $can_delete = Auth::user()->can('delete', $community_post);
         
-        return Inertia::render('Frontend/Posts/Show', compact('community', 'post', 'posts'));
+        return Inertia::render('Frontend/Posts/Show', compact('community', 'post', 'posts', 'can_update', 'can_delete'));
     }
 }
