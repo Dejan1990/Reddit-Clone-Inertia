beb48e3ba63991135b627ecb28cdaf092314f841
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-11-11 20:18:19 +0100

    dinamyc colors

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -49,4 +49,9 @@ class Post extends Model
     {
         return $this->belongsTo(Community::class);
     }
+
+    public function postVotes()
+    {
+        return $this->hasMany(PostVote::class);
+    }
 }
